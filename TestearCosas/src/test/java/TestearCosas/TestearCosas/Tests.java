2138cf20ecb7f4b463becb0df605b6a27c2ad94d
german <germanjongewaard@gmail.com>
2018-12-27 12:22:59 +0100

    TestingNew

---
+++ TestearCosas/src/test/java/TestearCosas/TestearCosas/Tests.java
@@ -0,0 +1,32 @@
+package TestearCosas.TestearCosas;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Tests {
+
+	
+	private int numeroUno = 5;
+	private int numeroDos = 3;
+	
+	@Test
+	public void testNumeroVerdadero() {
+		
+		Assert.assertTrue(numeroUno == numeroDos + 1, "El  nro. no es verdadero " + String.valueOf(numeroUno));		
+		
+	}
+	@Test
+	public void testNumeroFalso() {
+		
+		Assert.assertFalse(numeroUno == numeroDos, "Ambos nros. no son iguales"); //Va a dar falso ;-) 
+		
+	} 
+	
+	@Test
+	public void testNumerosIguales() {
+		
+		Assert.assertEquals(numeroUno + 10,  numeroDos + 12);
+		
+	}
+    
+}
