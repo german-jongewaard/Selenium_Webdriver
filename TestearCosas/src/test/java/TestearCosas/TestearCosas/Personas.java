e42621610c55669699ef894d0c35e6cd7a045a44
german <germanjongewaard@gmail.com>
2019-01-01 12:36:15 +0100

    HappyNewYearTesting

--- TestearCosas/src/test/java/TestearCosas/TestearCosas/Personas.java
+++ TestearCosas/src/test/java/TestearCosas/TestearCosas/Personas.java
@@ -5,7 +5,10 @@ public class Personas {
 	private String nombre;
 	private String apellido;
 	
-	public Personas() {
+	public Personas(String nombre, String apellido) {
+		
+		nombre = this.nombre;
+		apellido = this.apellido;
 		
 	}
 
